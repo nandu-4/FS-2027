83ca30f7b690f0e26a9411d6f58c51fc2283519b
varun sai <varunsaip2005@gmail.com>
2026-02-05 23:29:27 +0530

    Edited

--- DSA/A1/23rd_Jan/P5.cpp
+++ DSA/A1/23rd_Jan/P5.cpp
@@ -26,16 +26,3 @@ input=10
 output=-1
 */
 
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int main(){
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++) cin >> arr[i];
-    int count=0;
-    for(int i=0;i<)
-    return 0;
-}
